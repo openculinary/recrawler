37e252e8c237a782f1551de7560261df3b4ea00f
James Addison <james@reciperadar.com>
2019-10-14 15:43:40 +0000

    Implement duckduckgo recrawler

--- web/app.py
+++ web/app.py
@@ -1,12 +1,25 @@
+from duckpy import Client as ddg
 from flask import Flask, abort, jsonify, request
+from requests_futures.sessions import FuturesSession
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST'])
 def root():
-    url = request.args.getlist('products[]')
-    if not url:
+    products = request.args.getlist('products[]')
+    if not products:
         return abort(400)
 
-    return jsonify({})
+    products = ' '.join(products)
+    results = ddg().search(f'{products} recipes')
+    urls = [result['url'] for result in results]
+
+    session = FuturesSession()
+    for url in urls:
+        session.post(
+            url='http://api-service/api/recipes/crawl',
+            data={'url': url}
+        )
+
+    return jsonify(urls)

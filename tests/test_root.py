b8acee8578b89d0153100baabdb0395cb807c398
James Addison <james@reciperadar.com>
2019-11-25 15:38:37 +0000

    Add basic test coverage

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,2 +1,17 @@
-def test_request(client):
-    client.post('/')
+from mock import patch
+
+from duckpy import Client
+
+
+def test_empty_products(client):
+    response = client.post('/')
+
+    assert response.status_code == 400
+
+
+@patch.object(Client, 'search')
+def test_positive_query(mock_search, client):
+    response = client.post('/?products[]=tofu')
+
+    assert response.status_code == 200
+    mock_search.assert_called_with('tofu recipes')

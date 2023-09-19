5091296f83963dde3a7d161eae2989d761d46263
James Addison <james@reciperadar.com>
2023-09-19 17:06:22 +0100

    Add naive TTL-based query cache as a filter against repeated upstream querying

--- web/app.py
+++ web/app.py
@@ -1,7 +1,10 @@
+from cachetools import TTLCache
 from flask import Flask, abort, jsonify, request
 import httpx
 
 app = Flask(__name__)
+
+query_cache = TTLCache(maxsize=512, ttl=60 * 60 * 4)
 
 
 @app.route("/", methods=["POST"])
@@ -29,6 +32,11 @@ def root():
     query += " -".join([""] + exclude)
     query += " ".join([""] + equipment)
     query += " recipes"
+
+    # Use an in-process time-limited cache to filter repeat queries
+    if query in query_cache:
+        return abort(501)
+    query_cache[query] = True
 
     # TODO: Restore recipe recrawling
     urls = []

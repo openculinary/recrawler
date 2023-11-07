b83c32df902f25d54549232ead1d7bb796622478
James Addison <james@reciperadar.com>
2023-11-07 14:45:02 +0000

    Fixup: relocate validation logic

--- web/app.py
+++ web/app.py
@@ -17,6 +17,12 @@ def root():
     equipment = request.args.getlist("equipment[]")
     offset = request.args.get("offset", type=int, default=0)
 
+    # Forward up to five terms from each of the include and exclude lists;
+    # equipment terms are ignored currently, to increase the cache hit ratio
+    include = include[:5]
+    exclude = exclude[:5]
+    equipment = equipment[:0]
+
     # Ensure we can form a positive query
     if not include and not equipment:
         return abort(400)
@@ -24,12 +30,6 @@ def root():
     # Ignore pagination for now
     if offset > 0:
         return abort(501)
-
-    # Forward up to five terms from each of the include and exclude lists;
-    # equipment terms are ignored currently, to increase the cache hit ratio
-    include = include[:5]
-    exclude = exclude[:5]
-    equipment = equipment[:0]
 
     # Construct a web search query
     query = " ".join(include)
